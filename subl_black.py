1d15c69ef3006f7a115be38325f5a4c0e4081783
Stefan Lehmann <stefan.st.lehmann@gmail.com>
2018-10-19 18:06:36 +0200

    some settings

--- subl_black.py
+++ subl_black.py
@@ -1,7 +1,11 @@
+import logging
 import sublime
 import sublime_plugin
 import subprocess
 import os
+
+
+logger = logging.getLogger(__name__)
 
 
 class BlackCommand(sublime_plugin.TextCommand):
@@ -11,6 +15,7 @@ class BlackCommand(sublime_plugin.TextCommand):
         try:
             current_file = sublime.active_window().active_view().file_name()
             return current_file.endswith((".py", ".pyw"))
+
         except AttributeError:
             # catch AttributeError if sublime.active_window or active_view or file_name
             # are None, which can happen if there is no active window or no open file or
@@ -22,18 +27,21 @@ class BlackCommand(sublime_plugin.TextCommand):
     def run(self, edit):
 
         # get the filename for the current view
-        window = sublime.active_window()
-        view = window.active_view()
-        file_name = view.file_name()
+        try:
+            current_file = sublime.active_window().active_view().file_name()
+        except AttributeError as e:
+            logger.error(str(e))
+            return
 
         envs = os.environ.copy()
 
         p = subprocess.Popen(
-            ["black", file_name],
+            ["black", current_file],
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             env=envs,
         )
 
-        p.communicate()
+        stdout, stderr = p.communicate()
+        print(stdout, stderr)
