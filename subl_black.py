9cd50c0f1e3af944a3b0eaa8c3c2021d132e1f87
Stefan Lehmann <stefan.st.lehmann@gmail.com>
2018-10-20 23:06:30 +0200

    copy package data

--- subl_black.py
+++ subl_black.py
@@ -1,20 +1,68 @@
+import os
+import filecmp
+import sys
 import logging
 import sublime
 import sublime_plugin
 import subprocess
-import os
+import shutil
+import zipfile
 
-
+#: name of the plugin
+PLUGIN_NAME = "SublimeBlack"
+#: path where the plugin is meant to be installed
+INSTALLED_PLUGIN_NAME = "{0}.sublime-package".format(PLUGIN_NAME)
+#: package installation path
+PACKAGES_PATH = sublime.packages_path()
+#: plugin installation path
+PLUGIN_PATH = os.path.join(PACKAGES_PATH, PLUGIN_NAME)
+#: path of the plugin file
+INSTALLED_PLUGIN_PATH = os.path.abspath(os.path.dirname(__file__))
+#: settings filename
 SETTINGS = "SublimeBlack.sublime-settings"
 
 logger = logging.getLogger(__name__)
 
+sys.path.insert(0, PLUGIN_PATH)
+
 
 def plugin_loaded():
-    print("SublimeBlack loaded")
+    """Execute when Plugin is loaded.
+
+    Make sure all Plugin files are in the main package directory of Sublimetext.
+    This is mainly for the default settings file.
+
+    """
+    if INSTALLED_PLUGIN_PATH != PLUGIN_PATH:
+        # check if installed plugin is the same as this file
+        installed_plugin_path = os.path.join(PLUGIN_PATH, INSTALLED_PLUGIN_NAME)
+        if os.path.exists(installed_plugin_path) and filecmp.cmp(
+            installed_plugin_path, INSTALLED_PLUGIN_PATH
+        ):
+            return
+
+        # remove old package data
+        if os.path.exists(PLUGIN_PATH):
+            try:
+                shutil.rmtree(PLUGIN_PATH)
+            except:  # noqa: E722
+                logger.error("Could not remove old Plugin directory")
+
+        # create new plugin dir
+        if not os.path.exists(PLUGIN_PATH):
+            os.mkdir(PLUGIN_PATH)
+
+        z = zipfile.ZipFile(INSTALLED_PLUGIN_PATH, "r")
+        for f in z.namelist():
+            z.extract(f, PLUGIN_PATH)
+        z.close()
+
+        shutil.copyfile(INSTALLED_PLUGIN_PATH, installed_plugin_path)
 
 
 class Settings:
+    """Convenience class for accessing settings."""
+
     def __init__(self):
         try:
             self._settings = sublime.load_settings(SETTINGS)
@@ -29,7 +77,7 @@ class Settings:
             return None
 
 
-class BlackCommand(sublime_plugin.TextCommand):
+class black_reformat(sublime_plugin.TextCommand):
     """Black command for reformatting."""
 
     def is_enabled(self):
@@ -82,4 +130,8 @@ class BlackCommand(sublime_plugin.TextCommand):
         )
 
         stdout, stderr = p.communicate()
-        print(stdout, stderr)
+        logger.debug("{0}, {1}".format(stdout, stderr))
+
+
+class black_diff(sublime_plugin.TextCommand):
+    pass
