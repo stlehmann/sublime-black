cd247acd7e11c33be545ea43b09ef443aa4e6b7a
Stefan Lehmann <stefan.st.lehmann@gmail.com>
2018-10-20 20:00:44 +0200

    remove init error

--- subl_black.py
+++ subl_black.py
@@ -18,15 +18,12 @@ class Settings:
         except Exception:
             logger.error("Could not load settings")
 
-    def __get__(self, key):
+    def __getitem__(self, key):
         return self._settings.get(key)
 
 
 class BlackCommand(sublime_plugin.TextCommand):
     """Black command for reformatting."""
-
-    def __init__(self):
-        self.settings = Settings()
 
     def is_enabled(self):
         try:
@@ -42,7 +39,7 @@ class BlackCommand(sublime_plugin.TextCommand):
         return False
 
     def run(self, edit):
-
+        settings = Settings()
         # get the filename for the current view
         try:
             current_file = sublime.active_window().active_view().file_name()
@@ -54,19 +51,19 @@ class BlackCommand(sublime_plugin.TextCommand):
         args = []
 
         # line length: set maximum line length
-        if self.settings["line_length"] is not None:
-            args += ["-l{0}".format(self.settings["line_length"])]
+        if settings["line_length"] is not None:
+            args += ["-l{0}".format(settings["line_length"])]
 
         # py36: Allow using Python 3.6 syntax
-        if self.settings["py36"]:
+        if settings["py36"]:
             args += ["--py36"]
 
         # skip_string_normalization: Don't normalize string quotes or prefixes
-        if self.settings["skip_string_normalization"]:
+        if settings["skip_string_normalization"]:
             args += ["-S"]
 
         # skip_numeric_underscore_normalization: Don't normalize numeric underscores
-        if self.settings["skip_numeric_underscore_normalization"]:
+        if settings["skip_numeric_underscore_normalization"]:
             args += ["-N"]
 
         p = subprocess.Popen(
