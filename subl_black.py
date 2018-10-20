e1219ecf34bbe2ba102a2aa54c1d78016fd39429
Stefan Lehmann <stefan.st.lehmann@gmail.com>
2018-10-20 19:47:15 +0200

    add settings handling

--- subl_black.py
+++ subl_black.py
@@ -5,11 +5,25 @@ import subprocess
 import os
 
 
+SETTINGS = "SublimeBlack.sublime-settings"
+
 logger = logging.getLogger(__name__)
+
+
+class Settings:
+
+    def __init__(self):
+        self._settings = sublime.load_settings(SETTINGS)
+
+    def __get__(self, key):
+        return self._settings.get(key)
 
 
 class BlackCommand(sublime_plugin.TextCommand):
     """Black command for reformatting."""
+
+    def __init__(self):
+        self.settings = Settings()
 
     def is_enabled(self):
         try:
@@ -34,9 +48,26 @@ class BlackCommand(sublime_plugin.TextCommand):
             return
 
         envs = os.environ.copy()
+        args = []
+
+        # line length: set maximum line length
+        if self.settings["line_length"] is not None:
+            args += ["-l{0}".format(self.settings["line_length"])]
+
+        # py36: Allow using Python 3.6 syntax
+        if self.settings["py36"]:
+            args += ["--py36"]
+
+        # skip_string_normalization: Don't normalize string quotes or prefixes
+        if self.settings["skip_string_normalization"]:
+            args += ["-S"]
+
+        # skip_numeric_underscore_normalization: Don't normalize numeric underscores
+        if self.settings["skip_numeric_underscore_normalization"]:
+            args += ["-N"]
 
         p = subprocess.Popen(
-            ["black", current_file],
+            ["black", current_file] + args,
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
